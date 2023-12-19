0bbb6524d572a73cb72e98f937c89b5c5fcbe575
yassine <yassine.boukhateb@ensae.fr>
2023-12-19 21:50:15 +0100

    Create callback func

--- movie_recommender_app.py
+++ movie_recommender_app.py
@@ -41,6 +41,71 @@ app.layout = html.Div(
     style={'backgroundColor': '#00008080'}  # Utilisation du code hexadécimal pour un bleu foncé semi-transparent
 )
 
+@app.callback(
+    Output('output-recommendation', 'children'),
+    Input('recommend-button', 'n_clicks'),
+    [Input('movie-dropdown', 'value')]
+)
+def update_output(n_clicks, selected_movie_name):
+    if not selected_movie_name or selected_movie_name==" ":  # Si aucun film n'est sélectionné
+            # Créer une liste pour stocker les divs des 20 films populaires, 5 par ligne
+            popular_movies_divs = []
+            movies_per_row = 5  # Nombre de films par ligne
+            rows_count = len(top_20_popular_movies) // movies_per_row  # Nombre de lignes complètes
+            
+            for row in range(rows_count):
+                start_idx = row * movies_per_row
+                end_idx = start_idx + movies_per_row
+                row_movies = top_20_popular_movies.iloc[start_idx:end_idx]
+                
+                # Créer une ligne horizontale pour chaque série de 5 films populaires
+                movie_row = html.Div([
+                    html.Div([
+                        html.Div(row_movies.iloc[i]["title"]),  # Afficher le nom du film
+                        html.Img(src=row_movies.iloc[i]["poster_path"], style={'width': '100%', 'height': '15%'})
+                    ], style={'width': '20%', 'text-align': 'center', 'margin-right': '10px'})
+                    for i in range(movies_per_row)
+                ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'})
+                
+                # Ajouter la ligne horizontale des films populaires à la liste
+                popular_movies_divs.append(movie_row)
+            
+            return popular_movies_divs  # Affiche le nom des films ainsi que leur poster
+    else:
+        # Afficher le film sélectionné
+        selected_movie = movies_details[movies_details['title'] == selected_movie_name]
+        selected_movie_div = html.Div([
+            html.H2(selected_movie['title'].values[0]),
+            html.Img(src=selected_movie['poster_path'].values[0], style={'width': '20%', 'height': 'auto'}),
+            html.P(selected_movie['overview'].values[0])
+        ])
+
+        # Afficher les films similaires
+        recommended_movies, recommended_movies_posters = recommend(selected_movie_name)
+
+        recommendations = []
+        posters_per_row = 5  # Nombre de posters par ligne
+        rows_count = len(recommended_movies_posters) // posters_per_row  # Nombre de lignes complètes
+        
+        for row in range(rows_count):
+            start_idx = row * posters_per_row
+            end_idx = start_idx + posters_per_row
+            row_posters = recommended_movies_posters[start_idx:end_idx]
+            row_names = recommended_movies[start_idx:end_idx]
+        
+        # Créer une ligne horizontale de posters centrée
+            poster_row = html.Div([
+                html.Div([
+                    html.Div(row_names[i]),
+                    html.Img(src=row_posters[i], style={'width': '100%', 'height': 'auto'})
+                ], style={'width': '20%', 'text-align': 'center', 'margin-right': '10px'})
+                for i in range(posters_per_row)
+            ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'})
+        
+            # Ajouter la ligne horizontale de posters à la liste des recommandations
+            recommendations.append(poster_row)
+
+        return [selected_movie_div] + recommendations  # Retourner la div du film sélectionné avec les films similaires
 
 if __name__ == '__main__':
     app.run_server(debug=True)
